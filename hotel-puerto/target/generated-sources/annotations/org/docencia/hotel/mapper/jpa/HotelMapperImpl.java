ec0d9efd195147acf55895609288340246b4eeb4
nexphernandez <nicolaseh3b1112@gmail.com>
2025-12-25 21:02:59 +0000

    Se crea la estructura de proyecto con todas las clases e interfaces sin implementar

---
+++ hotel-puerto/target/generated-sources/annotations/org/docencia/hotel/mapper/jpa/HotelMapperImpl.java
@@ -0,0 +1,37 @@
+package org.docencia.hotel.mapper.jpa;
+
+import javax.annotation.processing.Generated;
+import org.docencia.hotel.domain.model.Hotel;
+import org.docencia.hotel.persistence.jpa.entity.HotelEntity;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-25T21:00:03+0000",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
+)
+@Component
+public class HotelMapperImpl implements HotelMapper {
+
+    @Override
+    public HotelEntity toEntity(Hotel domain) {
+        if ( domain == null ) {
+            return null;
+        }
+
+        HotelEntity hotelEntity = new HotelEntity();
+
+        return hotelEntity;
+    }
+
+    @Override
+    public Hotel toDomain(HotelEntity entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        Hotel hotel = new Hotel();
+
+        return hotel;
+    }
+}
---
+++ hotel-puerto/target/generated-sources/annotations/org/docencia/hotel/mapper/nosql/GuestPreferencesMapperImpl.java
@@ -0,0 +1,37 @@
+package org.docencia.hotel.mapper.nosql;
+
+import javax.annotation.processing.Generated;
+import org.docencia.hotel.domain.model.GuestPreferences;
+import org.docencia.hotel.persistence.nosql.document.GuestPreferencesDocument;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-25T21:00:04+0000",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
+)
+@Component
+public class GuestPreferencesMapperImpl implements GuestPreferencesMapper {
+
+    @Override
+    public GuestPreferencesDocument toDocument(GuestPreferences domain) {
+        if ( domain == null ) {
+            return null;
+        }
+
+        GuestPreferencesDocument guestPreferencesDocument = new GuestPreferencesDocument();
+
+        return guestPreferencesDocument;
+    }
+
+    @Override
+    public GuestPreferences toDomain(GuestPreferencesDocument doc) {
+        if ( doc == null ) {
+            return null;
+        }
+
+        GuestPreferences guestPreferences = new GuestPreferences();
+
+        return guestPreferences;
+    }
+}
